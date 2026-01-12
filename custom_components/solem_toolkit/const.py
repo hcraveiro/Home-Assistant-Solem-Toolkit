5322086ea1bba472e6c92b500f251866052c6139
hcraveiro <henrique.craveiro@gmail.com>
2026-01-12 15:53:59 +0000

    Added a few config keys

--- custom_components/solem_toolkit/const.py
+++ custom_components/solem_toolkit/const.py
@@ -1,1 +1,10 @@
+"""Constants for Solem Toolkit."""
+
 DOMAIN = "solem_toolkit"
+
+# Solem BLE characteristic used for command writes
+CHARACTERISTIC_UUID = "108b0002-eab5-bc09-d0ea-0b8f467ce8ee"
+
+# Default Bluetooth connection timeout (seconds)
+DEFAULT_BLUETOOTH_TIMEOUT = 15
+MIN_BLUETOOTH_TIMEOUT = 5
